4ee184dae73ea00e7d9749a9e8167b6a4f252d57
ahmed-elnogoumi <ahmedelnogoumi@gmail.com>
2026-05-19 21:58:22 -0700

    completed 06_repeatstring per spec
    completed the exercise per instructions provide in readme.md

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    let strCnct = [];
+    if (num >= 0){
+    for (let i = 0; i < num; i++) {
+        strCnct.push(word);
+        }
+    } else {
+        return "ERROR";
+    }
 
+    return strCnct.join("");
 };
 
 // Do not edit below this line
