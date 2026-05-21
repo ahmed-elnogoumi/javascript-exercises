fc90503e375ac11ac85e915ccb29f1d03f89db83
ahmed-elnogoumi <ahmedelnogoumi@gmail.com>
2026-05-20 17:25:44 -0700

    completed 10_leapyears
    completed leapYears.js per README.md spec

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // Checking if leap year conditions are true -- otherwise the statmeent goes down to false
+    console.log("The year is " + year);
+    console.log(year % 100);
+    console.log(year % 400);
+    if (year % 4 === 0 && !(year % 100 === 0)) {
+        return true;
+    } 
+    if (year % 100 === 0 && year % 400 === 0) {
+        return true;
+    }
 
+    return false;
 };
 
 // Do not edit below this line
