6658dd8e6ee891111bc29a5bb6c7b4ca69a2e660
ahmed-elnogoumi <ahmedelnogoumi@gmail.com>
2026-05-20 17:06:34 -0700

    completed 09_sumall
    completed sumAll.js per README.md; all tests pass

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let arrOfVals = [];
+    let totalVal = 0;
 
+    if ((a < 0 || b < 0) || 
+        (a % 1 !== 0 || b % 1 !== 0) || 
+        (typeof a !== "number" || typeof b !== "number")) {
+        return "ERROR";
+    }
+
+    // Flipping argument a and b if a is bigger than b
+    if (a > b) {
+        let temp = a;
+        a = b;
+        b = temp;
+    }
+
+
+    for (let i = a; i <= b; i++) {
+        arrOfVals.push(i);
+    }
+
+    arrOfVals.forEach((element, index) => {
+        totalVal += arrOfVals[index];
+    })
+
+    return totalVal;
 };
 
 // Do not edit below this line
