5baeeddd337754edb398962f51f353431acee218
ahmed-elnogoumi <ahmedelnogoumi@gmail.com>
2026-05-20 17:50:45 -0700

    Completed 11_tempconversion per spec
    Completed tempConversion.js per README.md spec

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let conversionResult = (fahrenheitTemp - 32) * 5/9;
+  return parseFloat(conversionResult.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let conversionResult = (celsiusTemp * 9/5 + 32);
+  return parseFloat(conversionResult.toFixed(1));
 };
 
 // Do not edit below this line
