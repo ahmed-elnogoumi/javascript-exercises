da5130c3a3ae49e97694966b94aaa811b6e8b381
ahmed-elnogoumi <ahmedelnogoumi@gmail.com>
2026-07-31 12:44:06 -0700

    Completed calculator.js in 12_calculator. All tests pass.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const initialValue = 0;
+  const sumWithInitial = array.reduce(
+    (accumulator, currentValue) => accumulator + currentValue, initialValue,
+  )
+	return sumWithInitial;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const initialValue = array[0];
+  const multiplyWithInitial = array.reduce(
+    (accumulator, currentValue) => accumulator * currentValue, 1
+  )
+  return multiplyWithInitial;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let x = 1;
+  if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    while (n > 1) {
+      x = (x * n);
+      n--;
+    }
+    return x;
+  }
 };
 
 // Do not edit below this line
