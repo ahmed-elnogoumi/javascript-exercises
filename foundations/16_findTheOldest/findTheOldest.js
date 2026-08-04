83ae32cac2004b53fe6bc6c7cdc4651f5c25ff01
ahmed-elnogoumi <ahmedelnogoumi@gmail.com>
2026-08-04 11:28:05 -0700

    Completed 16_findTheOldest per README.md specs.
    All test suites fully pass.

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson = undefined;
+    let currentDate = new Date();
 
+    people.forEach((person) => {
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = currentDate.getFullYear();
+        }
+    });
+    
+    people.forEach(function (person) {
+        if (oldestPerson === undefined) {
+            oldestPerson = person;
+        } 
+        
+        if ((person.yearOfDeath - person.yearOfBirth) > (oldestPerson.yearOfDeath - oldestPerson.yearOfBirth)) {
+            oldestPerson = person;
+        }
+    })
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
