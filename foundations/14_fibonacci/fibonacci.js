35c12c624fdbcf30cfed232d8b5a223b25c8adfa
ahmed-elnogoumi <ahmedelnogoumi@gmail.com>
2026-08-03 21:14:38 -0700

    Completed 14_fibonacci. All test suites pass.

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(i) {
+    let fib_array = [];
+    
+    if (typeof i === "string") {
+        i = parseInt(i);
+    }
 
+    if (i < 0) {
+        return "OOPS";
+    }
+    
+    if (i === 0) {
+        return 0;
+    }
+
+    for (let x = 0; x < i; x++) {
+        if (x === 0 || x === 1) {
+            fib_array.push(1);
+        } else {
+            fib_array.push(fib_array[x - 1] + fib_array[x - 2]);
+            console.log(fib_array);
+            console.log(x);
+        }
+    }
+
+    return fib_array.at(-1);
 };
 
 // Do not edit below this line
